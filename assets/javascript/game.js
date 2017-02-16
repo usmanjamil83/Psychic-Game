3b38e12264fa07c918365738d0e23114d79c0a27
usmanjamil83 <usmanjamil83@hotmail.com>
2017-02-16 17:58:14 -0500

    new commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,65 +1,66 @@
 var computerChoices = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
-
 var wins=0;
 var losses=0;
 var guesses=12;
 var guessesLeft=12;
 var guessedLetters=[];
-var letterToGuess = null;
 
 var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-console.log('computerGuess ' + computerGuess )
 
-var updateGuessesLeft = function () {
-	document.querySelector('#guessLeft').innerHTML = "Guesses Left:" + guessesLeft;
+// var newGuessesLeft = function () {
+// 	document.querySelector('#guessLeft').innerHTML = "Guesses Left:" + guessesLeft;
+// }
 
+// var newGuessedLetters = function() {
+// 	document.querySelector('#yourGuessesSoFar').innerHTML = "Your Guesses so far: " + guessedLetters.join(',');
+// }
+
+var newGuessesLeft = function () {
+	document.getElementById('guessesLeft').innerHTML = guessesLeft;
 }
 
-var updateLetterToGuess = function () {
-	this.letterToGuess = this.computerChoices[Math.floor(Math.random() * this.computerChoices.length)];
+var newGuessedLetters = function() {
+	document.getElementById('yourGuessesSoFar').innerHTML = guessedLetters.join(',');
 }
-
-var updateGuessesSoFar = function() {
-	document.querySelector('#soFar').innerHTML = "Your Guesses so far: " + guessedLetters.join(', ');
-};
 
 var reset = function() {
-	totalGuesses = 12;
+	computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+	guesses = 12;
 	guessesLeft = 12;
 	guessedLetters = [];
-
-	updateLetterToGuess();
-	updateGuessesLeft();
-	updateGuessesSoFar();
+	
+	newGuessesLeft();
+	newGuessedLetters();
 }
 
-updateLetterToGuess();
-updateGuessesLeft();
-
-
 document.onkeyup = function(event) {
-  guessesLeft--;
-  var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-  console.log('userGuess ' + userGuess)
+	guessesLeft--;
+	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+	console.log('computerGuess ' + computerGuess);
+	console.log('userGuess ' + userGuess);
 
-  guessedLetters.push(userGuess);
-  updateGuessesLeft();
-  updateGuessesSoFar();
+	guessedLetters.push(userGuess);	
+	newGuessesLeft();
+	newGuessedLetters();
 
-
-	if (guessesLeft > 0){
-		if (userGuess == computerGuess){
+	if (guessesLeft > 0)
+	{
+		if (userGuess == computerGuess)
+		{
 			wins++;
-			document.querySelector('#wins').innerHTML = "Wins: " + wins;
-
+			// document.querySelector('#wins').innerHTML = "Wins: " + wins;
+			document.getElementById('wins').innerHTML = wins;
 			reset();
 		}
-	}else if(guessesLeft == 0){
-            // Then we will loss and we'll update the html to display the loss 
-            losses++;
-            document.querySelector('#losses').innerHTML = "Losses: " + losses;
-
-            reset();
-        }
-    };
+	}
+	
+	else if(guessesLeft == 0)
+	{
+		losses++;
+		// document.querySelector('#losses').innerHTML = "Losses: " + losses;
+		document.getElementById('losses').innerHTML = losses;
+		reset();
+	}
+};
